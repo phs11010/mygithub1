55702afd4c8162450248a66b4c71f546cd180876
heung su <phs10110@naver.com>
2016-09-02 11:46:56 +0900

    프로젝트 공유

---
+++ Git_phs_0709/src/com/bitacademy/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.bitacademy.git;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¾È³ç git");
+		System.out.println("Á¡½É ¹¹ ¸Ô³Ä");
+		
+
+	}
+
+}
